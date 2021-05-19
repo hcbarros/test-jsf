e5e34a8bb4d566ac628552e948e273a0eabe91cd
hcbarros <hcb@a.recife.ifpe.edu.br>
2021-05-19 11:31:44 -0300

    commit

---
+++ src/main/java/br/com/usuarios_jsf/view/UserBean.java
@@ -0,0 +1,29 @@
+package br.com.usuarios_jsf.view;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+@Named 
+@RequestScoped
+public class UserBean {
+	
+	private String input;
+    private String output;
+
+    public void submit() {
+        output = "Hello World! You have typed: " + input;
+    }
+
+    public String getInput() {
+        return input;
+    }
+
+    public void setInput(String input) {
+        this.input = input;
+    }
+
+    public String getOutput() {
+        return output;
+    }
+
+}
